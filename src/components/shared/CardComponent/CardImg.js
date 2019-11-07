b368c75016c76077c16110da911f8cb7901e94a6
thesoundfromthesky <nagara@naver.com>
2019-11-07 12:59:59 -0600

    minor fix

--- src/components/shared/CardComponent/CardImg.js
+++ src/components/shared/CardComponent/CardImg.js
@@ -11,7 +11,7 @@ export default function CardImg({ url, explanation, media_type }) {
         <div className="card-iframe">
           <iframe title="youtube"
             width="100%"
-            src="https://www.youtube.com/embed/J4gJY_5eNqQ"
+            src={url}
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
